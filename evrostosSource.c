dd724caf2d94a913cab168520a18f276b4f6e45f
Tzanis Anevlavis <janis10@ucla.edu>
2018-10-27 13:33:58 -0700

    Update evrostosSource.c

--- evrostosSource.c
+++ evrostosSource.c
@@ -1,7 +1,7 @@
 /*
  *  Evrostos: The rLTL Verifier
  *  Authors: Tzanis Anevlavis, Daniel Neider, Matthew Philippe, Paulo Tabuada
- *  Copyright (C) 2018  UCLA
+ *  Copyright (C) 2018, Tzanis Anevlavis, Daniel Neider, Matthew Philippe, Paulo Tabuada
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
